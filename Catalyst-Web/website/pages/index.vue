6f108258c4d2d7b0d396ce18e8b29bffe69d726a
kybex11 <artm.22@@internet.ru>
2024-02-13 18:17:05 +0300

    Update: Beta navbar

--- Catalyst-Web/website/pages/index.vue
+++ Catalyst-Web/website/pages/index.vue
@@ -1,3 +1,64 @@
 <template>
-
+<div class="">
+  <br>
+  <div class="navbar">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Invite</a></li>
+      <li><a href="#">Dashboard</a></li>
+      <li class="profile-btn"><a href="#">Profile</a></li>
+    </ul>
+  </div>
+</div>
 </template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #242424;
+}
+
+.navbar {
+  box-shadow: 0 0 5px  white;
+  width: 90%;
+  height: 50px;
+  border-radius: 25px;
+  margin: 10px auto;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  height: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 16px;
+  text-decoration: none;
+  border-radius: 25px;
+
+}
+
+li a:hover {
+  background-color: #000;
+  color: white;
+}
+
+.profile-btn {
+  float: right;
+}
+
+.profile-btn a {
+  background: none;
+}
+</style>
