c73757529c4a736babfa50cd04f61faa4b75d016
kybex11 <artm.22@@internet.ru>
2024-02-15 17:55:02 +0300

    page example

--- Catalyst-Web/website/pages/Invite.vue
+++ Catalyst-Web/website/pages/Invite.vue
@@ -1,3 +1,10 @@
 <template>
-
+  <h1>This is invite</h1>
+  <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias nihil consequuntur modi natus quaerat, enim totam sed aspernatur eaque reiciendis! Cumque sapiente ea facilis placeat eius cum soluta, nihil nam.</h1>
 </template>
+
+<style>
+h1 {
+  color: white;
+}
+</style>
