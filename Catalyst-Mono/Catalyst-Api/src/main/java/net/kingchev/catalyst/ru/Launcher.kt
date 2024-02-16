410931cff740e7b20ea307da47a48eabc1ec09c7
KiNgchev <ketone.git@gmail.com>
2024-02-16 20:47:28 +0300

    Change method main to static

--- Catalyst-Mono/Catalyst-Api/src/main/java/net/kingchev/catalyst/ru/Launcher.kt
+++ Catalyst-Mono/Catalyst-Api/src/main/java/net/kingchev/catalyst/ru/Launcher.kt
@@ -1,12 +1,18 @@
 package net.kingchev.catalyst.ru
 
+import net.kingchev.catalyst.ru.config.CommonConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(
+    CommonConfiguration::class
+)
 class CatalystApi
 
 object Launcher {
+    @JvmStatic
     fun main(args: Array<String>) {
         runApplication<CatalystApi>(*args)
     }
