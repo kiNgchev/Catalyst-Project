961b3e1970634fabbb4f274a3ed19ac7a47bcf7f
KiNgchev <ketone.git@gmail.com>
2024-02-16 22:10:00 +0300

    (Catalyst-Reactor) Change rabbitmq connection factory to mono

--- Catalyst-Reactor/reactor/rabbitmq/connection_factory.py
+++ Catalyst-Reactor/reactor/rabbitmq/connection_factory.py
@@ -4,11 +4,7 @@ from pika import BlockingConnection, ConnectionParameters, PlainCredentials
 class RabbitMqConnectionFactory:
     @staticmethod
     def get_connection() -> BlockingConnection:
-        plain_credentials = PlainCredentials("gues", "guest")
-        connection_params = [
-                ConnectionParameters(host="localhost", port=5672, virtual_host="/", credentials=plain_credentials),
-                ConnectionParameters(host="localhost", port=5673, virtual_host="/", credentials=plain_credentials),
-                ConnectionParameters(host="localhost", port=5674, virtual_host="/", credentials=plain_credentials)
-        ]
+        plain_credentials = PlainCredentials("guest", "guest")
+        connection_params = ConnectionParameters(host="localhost", port=5672, virtual_host="/", credentials=plain_credentials)
         connection = BlockingConnection(connection_params)
         return connection
