d433a9e55e4676077d64f1ba16a47b2e0b8fc144
KiNgchev <ketone.git@gmail.com>
2024-02-16 20:49:08 +0300

    Update REST controllers

--- Catalyst-Mono/Catalyst-Api/src/main/java/net/kingchev/catalyst/ru/controller/base/BaseDashboardController.kt
+++ Catalyst-Mono/Catalyst-Api/src/main/java/net/kingchev/catalyst/ru/controller/base/BaseDashboardController.kt
@@ -1,7 +1,10 @@
 package net.kingchev.catalyst.ru.controller.base
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("baseDashboardController")
+@RequestMapping("/dashboard")
 abstract class BaseDashboardController : BaseRestController() {
 }
