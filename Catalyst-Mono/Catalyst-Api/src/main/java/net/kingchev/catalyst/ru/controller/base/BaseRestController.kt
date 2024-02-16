9698594b322b73bc04511393e3951ff66691af6f
KiNgchev <ketone.git@gmail.com>
2024-02-16 19:43:23 +0300

    (Catalyst-Mono) Create base controllers
    Create 3 base controllers:
    - Create BaseRestController for all REST controllers
    - Create BaseDashboardController for all dashboard REST controllers
    - Create BaseUserController all user REST controllers

---
+++ Catalyst-Mono/Catalyst-Api/src/main/java/net/kingchev/catalyst/ru/controller/base/BaseRestController.kt
@@ -0,0 +1,7 @@
+package net.kingchev.catalyst.ru.controller.base
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("baseRestController")
+abstract class BaseRestController {
+}
---
+++ Catalyst-Mono/Catalyst-Api/src/main/java/net/kingchev/catalyst/ru/controller/base/BaseUserController.kt
@@ -0,0 +1,7 @@
+package net.kingchev.catalyst.ru.controller.base
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("baseUserController")
+abstract class BaseUserController : BaseRestController() {
+}
