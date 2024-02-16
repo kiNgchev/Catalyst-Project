27a9c81132a2ac4378085cf25ec96883efdff6ac
kybex11 <artm.22@@internet.ru>
2024-02-16 19:51:16 +0300

    Added navbar to /dashboard

--- Catalyst-Web/website/pages/Dashboard.vue
+++ Catalyst-Web/website/pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <template>
+  <Navbar/>
   <h1>This is dashboard</h1>
   <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias nihil consequuntur modi natus quaerat, enim totam sed aspernatur eaque reiciendis! Cumque sapiente ea facilis placeat eius cum soluta, nihil nam.</h1>
 </template>
