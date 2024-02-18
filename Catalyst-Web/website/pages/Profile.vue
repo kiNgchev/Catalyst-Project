c8ae4d0f216b6e1ef03f7f21d40e2f0be0b621c0
kybex11 <artm.22@@internet.ru>
2024-02-18 10:51:06 +0300

    Profile Change

--- Catalyst-Web/website/pages/Profile.vue
+++ Catalyst-Web/website/pages/Profile.vue
@@ -3,10 +3,36 @@
   <Navbar/>
   <br>
   <div class="container">
-
+    <div class="profile-info">
+      <img src="/avatars/OpenStationDev.png" alt="" class="profile-logo">
+      <p>OpenStationDev</p>
+    </div>
   </div>
 </div>
 </template>
 
 <style>
+.container {
+  align-content: center;
+  text-align: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.profile-info {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  color: white;
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+
+.profile-logo {
+  height: 100px;
+  width: 100px;
+  border-radius: 100%;
+  margin-right: 20px;
+}
 </style>
