1b7ec62edd97efdb9a8087f161c3f0d23dae04ae
KiNgchev <ketone.git@gmail.com>
2024-03-12 18:20:29 +0300

    (Catalyst-Mono) Update AbstractCommand

--- Catalyst-Mono/Catalyst-Core/src/main/kotlin/net/kingchev/catalyst/ru/discord/command/model/AbstractCommand.kt
+++ Catalyst-Mono/Catalyst-Core/src/main/kotlin/net/kingchev/catalyst/ru/discord/command/model/AbstractCommand.kt
@@ -1,18 +1,23 @@
 package net.kingchev.catalyst.ru.discord.command.model
 
-import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.entities.Member
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.kingchev.catalyst.ru.core.localezied.service.LocaleService
 import net.kingchev.catalyst.ru.core.model.CommandStatus
+import net.kingchev.catalyst.ru.core.service.GuildConfigService
 import net.kingchev.catalyst.ru.discord.command.service.InternalCommandService
 import net.kingchev.catalyst.ru.discord.context.model.MessageContext
+import net.kingchev.catalyst.ru.discord.message.service.MessageService
 import org.springframework.beans.factory.annotation.Autowired
 
 abstract class AbstractCommand : Command {
     @Autowired
-    private lateinit var internalCommandService: InternalCommandService
+    protected lateinit var internalCommandService: InternalCommandService
+
+    @Autowired
+    protected lateinit var localeService: LocaleService
+
+    @Autowired
+    protected lateinit var messageService: MessageService
 
     open override fun isAvailable(event: MessageReceivedEvent, context: MessageContext): CommandStatus {
         val annotation = getAnnotation()
