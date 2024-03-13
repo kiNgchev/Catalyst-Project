f04fb576e93f73d99114f712e21d0b9be72181c7
kybex11 <artm.22@internet.ru>
2024-03-13 13:58:01 +0100

    Added forum and added ESLint

---
+++ Catalyst-Web/website/.eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended',
+    ],
+}
--- Catalyst-Web/website/pages/index.vue
+++ Catalyst-Web/website/pages/index.vue
@@ -63,7 +63,7 @@
         <br>
         <div class="developer-profile-container">
           <img src="/avatars/kiNgchev.gif" alt="kiNgchev" class="developers-picture">
-          <h2>ㅤㅤkiNgchev(KINGCHEV)ㅤ</h2><p>- Founder, Owner, Down, Backend Developer</p>
+          <h2>ㅤㅤkiNgchev(KINGCHEV)ㅤ</h2><p>- Founder, Owner, Backend Developerㅤㅤㅤ</p>
         </div>
         <br/>
         <div class="developer-profile-container">
