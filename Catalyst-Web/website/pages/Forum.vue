3c0e034ecd231b3c8599716a9e6f954bd2e7a22d
kybex11 <artm.22@internet.ru>
2024-03-14 17:30:20 +0100

    V1 Of Forum

--- Catalyst-Web/website/pages/Forum.vue
+++ Catalyst-Web/website/pages/Forum.vue
@@ -1,2 +1,32 @@
 <template>
+    <div class="side">
+        <div class="side-components">
+            <br>
+            <h1>Catalyst</h1>
+            <h2>Forum</h2>
+            <button>Ask question</button>
+        </div>
+    </div>
 </template>
+
+<style>
+.side {
+    width: 200px;
+    height: 10000px;
+    background-color: black;
+}
+
+.side-components {
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+.side-components h1 {
+    color: white;
+}
+
+.side-components h2 {
+    color: rgb(166, 166, 166);
+}
+</style>
