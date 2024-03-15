00aa501490c742276e4bf6469fb090c1c9959aba
KiNgchev <ketone.git@gmail.com>
2024-03-15 21:59:49 +0300

    (Catalyst-Mono) Update Ping command

--- Catalyst-Mono/Catalyst-Worker/src/main/kotlin/net/kingchev/catalyst/ru/worker/commands/misc/PingCommand.kt
+++ Catalyst-Mono/Catalyst-Worker/src/main/kotlin/net/kingchev/catalyst/ru/worker/commands/misc/PingCommand.kt
@@ -9,6 +9,7 @@ import net.kingchev.catalyst.ru.core.utils.LocaleUtils
 import net.kingchev.catalyst.ru.discord.command.model.AbstractCommand
 import net.kingchev.catalyst.ru.discord.command.model.CatalystCommand
 import net.kingchev.catalyst.ru.discord.context.model.MessageContext
+import net.kingchev.catalyst.ru.discord.context.model.SlashContext
 import java.util.Locale
 
 @CatalystCommand(
@@ -21,7 +22,7 @@ class PingCommand : AbstractCommand() {
     override fun build(): SlashCommandData {
         val locales = hashMapOf<DiscordLocale, String>()
         locales[DiscordLocale.RUSSIAN] = localeService.getMessage(getAnnotation().description, "ru")
-        val description = localeService.getMessage(getAnnotation().description, LocaleUtils.DEFAULT!!.language)
+        val description = localeService.getMessage(getAnnotation().description, LocaleUtils.DEFAULT.language)
         val builder = Commands.slash(
             getAnnotation().key,
             description
@@ -40,11 +41,11 @@ class PingCommand : AbstractCommand() {
         return true
     }
 
-    override fun execute(event: SlashCommandInteractionEvent): Boolean {
+    override fun execute(event: SlashCommandInteractionEvent, context: SlashContext): Boolean {
         val restPing = event.jda.restPing.complete()
         val gatewayPing = event.jda.gatewayPing
-        val embed = messageService.getBaseEmbed(localeService.getMessage("discord.command.ping.title", "en"))
-            .setDescription(localeService.getMessage("discord.command.ping.description", "en", restPing, gatewayPing))
+        val embed = messageService.getBaseEmbed(localeService.getMessage("discord.command.ping.title", context.locale))
+            .setDescription(localeService.getMessage("discord.command.ping.description", context.locale, restPing, gatewayPing))
             .build()
         event.interaction.replyEmbeds(embed).queue()
         return true
