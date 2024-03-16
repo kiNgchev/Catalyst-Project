36176dafd5355542a9ac67617be3078939fb0318
kybex11 <artm.22@internet.ru>
2024-03-16 07:17:05 +0100

    (Catalyst-Web) Updated content on forum_view_controller

--- Catalyst-Web/website/components/forum_view_controller.vue
+++ Catalyst-Web/website/components/forum_view_controller.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="forum_view_controller">
         <tr>
+            <td>{{ thread.title }}</td>
+            <br>
+            <td>{{ thread.description}}</td>
+            <br>
+            <td><button>View thread</button></td>
         </tr>
     </div>
 </template>
+<script>
+export default {
+    props: {
+        thread: Object
+    }
+}
+</script>
