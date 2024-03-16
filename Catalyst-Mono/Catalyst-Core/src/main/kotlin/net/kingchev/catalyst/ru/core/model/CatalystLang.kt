7fb4c2994558c91a9d9cf05a57b0c66a080089f4
KiNgchev <ketone.git@gmail.com>
2024-03-16 09:04:13 +0300

    (Catalyst-Mono) Update languages enum

--- Catalyst-Mono/Catalyst-Core/src/main/kotlin/net/kingchev/catalyst/ru/core/model/CatalystLang.kt
+++ Catalyst-Mono/Catalyst-Core/src/main/kotlin/net/kingchev/catalyst/ru/core/model/CatalystLang.kt
@@ -1,19 +1,17 @@
 package net.kingchev.catalyst.ru.core.model
 
-import java.nio.charset.Charset
 import java.util.*
 
 enum class CatalystLang(
-    var language: String,
-    var country: String,
-    nativeName: String,
+    val language: String,
+    val country: String,
+    val nativeName: String,
     var englishName: String,
     vararg val other: String
 ) {
     EN_US("en", "US", "English", "English"),
     RU_RU("ru", "RU", "Русский", "Russian", "cyka blyat", "cheeki breeki", "rush b");
 
-    val nativeName = String(nativeName.toByteArray(), Charset.forName("UTF-8"))
     val locale: Locale = Locale(language, country)
     val code: String = language + "_" + country
 }
