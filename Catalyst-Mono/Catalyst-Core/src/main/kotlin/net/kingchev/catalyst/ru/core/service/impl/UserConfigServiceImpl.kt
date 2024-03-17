e239cda8fb483ea1b4fc4793f3ac2bac3d9533b2
KiNgchev <ketone.git@gmail.com>
2024-03-17 22:20:21 +0300

    (Catalyst-Mono) Clear user config service

--- Catalyst-Mono/Catalyst-Core/src/main/kotlin/net/kingchev/catalyst/ru/core/service/impl/UserConfigServiceImpl.kt
+++ Catalyst-Mono/Catalyst-Core/src/main/kotlin/net/kingchev/catalyst/ru/core/service/impl/UserConfigServiceImpl.kt
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.annotation.CacheConfig
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.CachePut
-import org.springframework.cglib.core.Local
 import org.springframework.stereotype.Service
 
 @Service
